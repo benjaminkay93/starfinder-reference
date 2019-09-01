400c1639722f3ef4ab49d22b01e89daca2f7fe57
benjaminkay93 <ben.kay01@hotmail.co.uk>
2019-09-01 16:00:16 +0100

    add next with styled components

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: red;
+`;
+
+const Index = () => (
+  <div>
+    <Title>Hello, World!</Title>
+  </div>
+)
+
+export default Index
